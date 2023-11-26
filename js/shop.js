afb77ca09cfc73395c569232c69905febeae7cff
Milan Popović <poppovic.contact@gmail.com>
2023-11-26 01:29:03 +0100

    Validators added.

--- js/shop.js
+++ js/shop.js
@@ -3,6 +3,9 @@ let cart = document.getElementById('cart');
 let checkout = document.getElementById('shopping-cart');
 let save_info = document.getElementById('save-info');
 let cancel_info = document.getElementById('cancel');
+let credit_card = document.getElementById('card');
+let cash_on_delivery = document.getElementById('cash-on-delivery');
+let proceed;
 let index = 0;
 let inputs = [
     document.getElementById('first-name'),
@@ -16,12 +19,12 @@ let inputs = [
 ]
 
 function AddToCart(button){
-    index++;
     let product = {
         name: button.parentElement.getElementsByTagName('h1')[0].innerHTML,
         price: button.parentElement.getElementsByTagName('h2')[0].innerHTML.split('€')[0],
         quantity: input.value
     }
+    index++;
     localStorage.setItem(`${product.name + index}`, JSON.stringify(product));
     cart.innerHTML = ``;
     LoadCart();
@@ -52,22 +55,13 @@ function Total(total){
 }
 
 function CheckOutTotal(total){
-    if(page != "index.html"){
-        checkout.innerHTML += `
+    checkout.innerHTML += `
     <div class="checkout-cart-total">
         <h1>Total: ${total}€</h1>
-        <a href="../pages/checkout.html"><button id="checkout-my-cart">PROCEED</button></a>
+        <a href="#"><button id="checkout-my-cart" onclick="ProceedCheck();">PROCEED</button></a>
     </div>
     `;
-    }
-    else{
-        checkout.innerHTML += `
-    <div class="checkout-cart-total">
-        <h1>Total: ${total}€</h1>
-        <a href="./pages/checkout.html"><button id="checkout-my-cart">PROCEED</button></a>
-    </div>
-    `;
-    }
+    proceed = document.getElementById('checkout-my-cart');
 }
 
 function LoadCart(){
@@ -145,21 +139,32 @@ function CheckOut(){
 }
 
 save_info.onclick = () => {
-    let shipping = {
-        firstname:document.getElementById('first-name').value,
-        lastname:document.getElementById('last-name').value,
-        phonenumber:document.getElementById('phone-number').value,
-        email:document.getElementById('e-mail').value,
-        address:document.getElementById('address').value,
-        homenumber:document.getElementById('home-number').value,
-        city:document.getElementById('city').value,
-        postal:document.getElementById('postal-code').value
+    let b = false;
+    inputs.forEach((element)=>{
+        if(element.value == ""){
+            element.style.border = '1px solid red';
+            element.nextElementSibling.innerHTML = "Ovo polje je obavezno!";
+            b = true;
+        }
+    })
+    if(b == false){
+        let shipping = {
+            firstname:document.getElementById('first-name').value,
+            lastname:document.getElementById('last-name').value,
+            phonenumber:document.getElementById('phone-number').value,
+            email:document.getElementById('e-mail').value,
+            address:document.getElementById('address').value,
+            homenumber:document.getElementById('home-number').value,
+            city:document.getElementById('city').value,
+            postal:document.getElementById('postal-code').value
+        }
+        localStorage.setItem("shipping", JSON.stringify(shipping));
+        save_info.innerHTML = "Saved!";
+        setTimeout(() => {
+            save_info.innerHTML = "Save and Deliver Here";
+        }, 2000);
+        ProceedCheck();
     }
-    localStorage.setItem("shipping", JSON.stringify(shipping));
-    save_info.innerHTML = "Saved!";
-    setTimeout(() => {
-        save_info.innerHTML = "Save and Deliver Here";
-    }, 2000);
 }
 
 cancel_info.onclick = () => {
@@ -178,5 +183,149 @@ function LoadShippingAddress(){
             element.value = "";
         })
     }
+    ProceedCheck();
 }
 
+inputs[0].addEventListener("focusout", () => {
+    let firstNameRegex = /^[a-zA-Z]{3,30}$/;
+    if(inputs[0].value.match(firstNameRegex)){
+        inputs[0].style.border = '1px solid black';
+        inputs[0].nextElementSibling.innerHTML = "";
+    }
+    else{
+        inputs[0].style.border = '1px solid red';
+        inputs[0].nextElementSibling.innerHTML = "Netacan unos!<br>Minimum 3 karatera, maksimum 30 karaktera, bez brojeva i karaktera!";
+    }
+})
+
+inputs[1].addEventListener("focusout", () => {
+    let firstNameRegex = /^[a-zA-Z]{3,30}$/;
+    if(inputs[1].value.match(firstNameRegex)){
+        inputs[1].style.border = '1px solid black';
+        inputs[1].nextElementSibling.innerHTML = "";
+    }
+    else{
+        inputs[1].style.border = '1px solid red';
+        inputs[1].nextElementSibling.innerHTML = "Netacan unos!<br>Minimum 3 karatera, maksimum 30 karaktera, bez brojeva i karaktera!";
+    }
+})
+
+inputs[2].addEventListener("focusout", () => {
+    let PhoneNumberRegex = /^[1-9][0-9]{7,8}$/;
+    if(inputs[2].value.match(PhoneNumberRegex)){
+        inputs[2].style.border = '1px solid black';
+        inputs[2].nextElementSibling.innerHTML = "";
+    }
+    else{
+        inputs[2].style.border = '1px solid red';
+        inputs[2].nextElementSibling.innerHTML = "Netacan unos!<br>Unos treba biti bez pocetne nule, imati od 7 do 8 cifara i treba biti bez razmaka!";
+    }
+})
+
+inputs[3].addEventListener("focusout", () => {
+    let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if(inputs[3].value.match(emailRegex)){
+        inputs[3].style.border = '1px solid black';
+        inputs[3].nextElementSibling.innerHTML = "";
+    }
+    else{
+        inputs[3].style.border = '1px solid red';
+        inputs[3].nextElementSibling.innerHTML = "Netacan unos!<br>Unos treba biti u formatu npr.(johndoe@gmail.com)";
+    }
+})
+
+inputs[4].addEventListener("focusout", ()=>{
+    if(inputs[4].value != ""){
+        inputs[4].style.border = '1px solid black';
+        inputs[4].nextElementSibling.innerHTML = "";
+    }
+    else{
+        inputs[4].style.border = '1px solid red';
+        inputs[4].nextElementSibling.innerHTML = "Netacan unos!<br>";
+    }
+})
+
+inputs[5].addEventListener("focusout", ()=>{
+    let homenumberRegex = /^[0-9]+$/;
+    if(inputs[5].value.match(homenumberRegex)){
+        inputs[5].style.border = '1px solid black';
+        inputs[5].nextElementSibling.innerHTML = "";
+    }
+    else{
+        inputs[5].style.border = '1px solid red';
+        inputs[5].nextElementSibling.innerHTML = "Netacan unos!<br>";
+    }
+})
+
+inputs[6].addEventListener("focusout", ()=>{
+    if(inputs[6].value != ""){
+        inputs[6].style.border = '1px solid black';
+        inputs[6].nextElementSibling.innerHTML = "";
+    }
+    else{
+        inputs[6].style.border = '1px solid red';
+        inputs[6].nextElementSibling.innerHTML = "Netacan unos!<br>";
+    }
+})
+
+inputs[7].addEventListener("focusout", ()=>{
+    let postalRegex = /^\d{5}$/;
+    if(inputs[7].value.match(postalRegex)){
+        inputs[7].style.border = '1px solid black';
+        inputs[7].nextElementSibling.innerHTML = "";
+    }
+    else{
+        inputs[7].style.border = '1px solid red';
+        inputs[7].nextElementSibling.innerHTML = "Netacan unos!<br>";
+    }
+})
+
+credit_card.onclick = () => {
+    credit_card.dataset.selected = "true";
+    cash_on_delivery.dataset.selected = "false";
+    credit_card.innerHTML = "Credit Card " + "<i class='fa-solid fa-check'></i>";
+    cash_on_delivery.innerHTML = "Cash On Delivery";
+    credit_card.style.border = "1px solid var(--dg)";
+    cash_on_delivery.style.border = "none";
+}
+
+cash_on_delivery.onclick = () => {
+    credit_card.dataset.selected = "false";
+    cash_on_delivery.dataset.selected = "true";
+    cash_on_delivery.innerHTML = "Cash On Delivery " + "<i class='fa-solid fa-check'></i>";
+    credit_card.innerHTML = "Credit Card";
+    credit_card.style.border = "none";
+    cash_on_delivery.style.border = "1px solid var(--dg)";
+}
+
+function ProceedCheck(){
+    let b = false;
+    inputs.forEach((element)=>{
+        if(element.value == ""){
+            b = true;
+            element.style.border = '1px solid red';
+            element.nextElementSibling.innerHTML = "Ovo polje je obavezno!";
+            proceed.parentElement.href = "#";
+            proceed.setAttribute("disabled", "disabled");
+        }
+    })
+    if((b == false && credit_card.dataset.selected == "true" && cart_number.innerHTML != "0") || (b == false && cash_on_delivery.dataset.selected == "true" && cart_number.innerHTML != "0")){
+        proceed.parentElement.href = "../pages/checkout_proceed.html";
+        proceed.removeAttribute("disabled");
+    }
+}
+
+function Proceeded(){
+    document.getElementsByTagName('h1')[0].innerHTML = `Vasa narudzbina je pod brojem: XXX, proverite vas e-mail za vise detalja!`;
+    let products = [];
+    Object.keys(localStorage).forEach((element)=>{
+        products.push(element);
+     });
+    let index = products.indexOf("shipping");
+    if (index > -1) {
+        products.splice(index, 1);
+    }
+    products.forEach((element)=>{
+        localStorage.removeItem(`${element}`);
+    })
+}
